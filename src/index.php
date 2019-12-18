22f72f5ca063a7ecb6647552f1d9f5e8392cf000
gerasimovs <mrteos@gmail.com>
2019-12-18 13:38:21 +0700

    Wrap into function and add input parameters

--- src/index.php
+++ src/index.php
@@ -1,31 +1,36 @@
 <?php
 
-/* Not optimal */
-for ($i = 1; $i <= 100; $i++) {
-    if ($i % 3 === 0)
-        echo 'Fizz';
-        
-    if ($i % 5 === 0)
-        echo 'Buzz';
-        
-    if ($i % 3 && $i % 5)
-        echo $i;
-    
-    echo PHP_EOL;
+function fizzBuzz($start, $end) {
+    /* Not optimal */
+    for ($i = $start; $i <= $end; $i++) {
+        if ($i % 3 === 0)
+            echo 'Fizz';
+
+        if ($i % 5 === 0)
+            echo 'Buzz';
+
+        if ($i % 3 && $i % 5)
+            echo $i;
+
+        echo PHP_EOL;
+    }
 }
 
-/* Optimal */
-for ($i = 1; $i <= 100; $i++) {
-    if ($i % 15 === 0) {
-        /* $i % 3 === 0 && $i % 5 === 0 */
-        echo 'FizzBuzz';
-    } elseif ($i % 3 === 0) {
-        echo 'Fizz';
-    } elseif ($i % 5 === 0) {
-        echo 'Buzz';
-    } else {
-        echo $i;
+
+function fizzBuzzOptimal($start, $end) {
+    /* Optimal */
+    for ($i = $start; $i <= $end; $i++) {
+        if ($i % 15 === 0) {
+            /* $i % 3 === 0 && $i % 5 === 0 */
+            echo 'FizzBuzz';
+        } elseif ($i % 3 === 0) {
+            echo 'Fizz';
+        } elseif ($i % 5 === 0) {
+            echo 'Buzz';
+        } else {
+            echo $i;
+        }
+
+        echo PHP_EOL;
     }
-    
-    echo PHP_EOL;
 }
